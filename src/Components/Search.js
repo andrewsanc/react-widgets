81ebf3142773d8caeeb60405862c5ad600e6773d
TB831 <druu831@gmail.com>
2021-07-19 13:17:11 -0700

    thottle requests and added delayed to request so that we dont exhaust resources. Added a check for initial rendering

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -20,16 +20,29 @@ const Search = () => {
       setSearchResults(data.query.search);
     }
 
-    if (searchTerm) {
+    if (searchTerm && !searchResults.length) {
       search();
+    } else {
+      var delayedRequest = setTimeout(() => {
+        if (searchTerm) {
+          search();
+        }
+      }, 1000);
+    }
+
+    return () => {
+      clearTimeout(delayedRequest);
     }
   }, [searchTerm]);
 
   const renderedResults = searchResults.map(resultObj => {
     return (
       <div key={resultObj.pageid} className='item'>
+        <div className='right floated content'>
+          <a className='ui button' href={`https://en.wikipedia.org?curid=${resultObj.pageid}`}>Visit</a>
+        </div>
         <div className='content'>
-          <div className='header'>{resultObj.title}</div>
+          <div className='header'><a href={`https://en.wikipedia.org?curid=${resultObj.pageid}`}>{resultObj.title}</a></div>
           <span dangerouslySetInnerHTML={{ __html: resultObj.snippet}}></span>
         </div>
       </div>
